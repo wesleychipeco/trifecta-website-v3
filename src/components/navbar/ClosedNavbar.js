1c47d8793a59300d5398e0d026aeea2ea0ac9dee
Wesley Chipeco <wescollege36@gmail.com>
2022-11-14 15:14:08 -0800

    Do not show navbar when on home page

--- src/components/navbar/ClosedNavbar.js
+++ src/components/navbar/ClosedNavbar.js
@@ -2,9 +2,16 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import * as S from "styles/Navbar.styles";
 import { openNavbar } from "store/navbarSlice";
+import { useLocation } from "react-router-dom";
 
 export const ClosedNavbar = () => {
   const dispatch = useDispatch();
+  const location = useLocation();
+
+  if (location.pathname === "/") {
+    return null;
+  }
+
   return (
     <S.ClosedNavbarContainer onClick={() => dispatch(openNavbar())}>
       <S.ClosedBars icon="bars" />
