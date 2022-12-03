8aa944c6515936a80aa3acb88fc5d184abfde5b4
Wesley Chipeco <wescollege36@gmail.com>
2022-12-02 17:52:39 -0800

    Scrape/display logic for basketball standings

--- src/screens/dynasty/DynastyBasketballStandings.js
+++ src/screens/dynasty/DynastyBasketballStandings.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import isSameDay from "date-fns/isSameDay";
 import { returnMongoCollection } from "database-management";
 import * as S from "styles/StandardScreen.styles";
 import { Table } from "components/table/Table";
@@ -23,31 +24,73 @@ export const DynastyBasketballStandings = () => {
   });
 
   useEffect(() => {
+    const display = async (dynastyStandings, divisionStandings) => {
+      setDynastyStandings(dynastyStandings);
+      setDivisionStandings(divisionStandings);
+    };
+
     // TODO extract out scrape & calculate dyansty points functions
-    const scrape = async () => {
+    const scrape = async (collection) => {
       const leagueId = "aznbe7wvl8esmlyo"; // basketball
       // const leagueId = "2b0xp4cqkk5ztl6x"; // baseball
       const tableStandings = await standingsScraper(leagueId);
-      const globalDivisionStandings = formatScrapedStandings(tableStandings);
-      console.log("gds", globalDivisionStandings);
-
-      setDivisionStandings(globalDivisionStandings);
-      setDynastyStandings(
-        assignRankPoints(
-          Object.values(globalDivisionStandings).flat(1),
-          "winPer",
-          HIGH_TO_LOW,
-          "dynastyPoints",
-          16,
-          1
-        )
+      const divisionStandings = formatScrapedStandings(tableStandings);
+      const dynastyStandings = assignRankPoints(
+        Object.values(divisionStandings).flat(1),
+        "winPer",
+        HIGH_TO_LOW,
+        "dynastyPoints",
+        16,
+        1
       );
+
+      display(dynastyStandings, divisionStandings);
+
+      // delete, then save to mongodb
+      console.log("Delete, then save to mongodb");
+      await collection.deleteMany({ year });
+      await collection.insertOne({
+        year,
+        lastScraped: new Date().toISOString(),
+        dynastyStandings,
+        divisionStandings,
+      });
     };
 
-    scrape();
+    const check = async () => {
+      const collection = await returnMongoCollection(
+        "basketballStandings",
+        era
+      );
+      const data = await collection.find({ year });
+      const object = data?.[0] ?? {};
+      const {
+        lastScraped: lastScrapedString,
+        dynastyStandings,
+        divisionStandings,
+      } = object;
+
+      // if no last scraped string, always scrape
+      if (!lastScrapedString) {
+        scrape(collection);
+      } else {
+        const alreadyScraped = isSameDay(
+          new Date(),
+          new Date(lastScrapedString)
+        );
+
+        // if alreadyd scraped today, just display
+        if (alreadyScraped) {
+          display(dynastyStandings, divisionStandings);
+        } else {
+          scrape(collection);
+        }
+      }
+    };
+
+    check();
   }, []);
 
-  console.log("hi", divisionStandings);
   return (
     <S.FlexColumnCenterContainer>
       <S.Title>{`${year} Basketball Standings for ${era}`}</S.Title>
